4901b1ae9d7cdf467ac3700896ce04f70c975abd
nawfalchidd <nawfalchiddi@icloud.com>
2026-04-20 05:50:16 -0700

    Exercice 8- menu

---
+++ CHD8.sh
@@ -0,0 +1,22 @@
+while true
+do
+	echo "------"
+	echo "1.Hello"
+	echo "2.Show"
+	echo "3.Exit"
+	echo "------"
+	read choice
+	if [ $choice = 1 ]
+	then
+		echo "Hello"
+	elif [ $choice = 2 ]
+	then
+		echo ""
+	elif [ $choice = 3 ]
+	then
+		echo "exiting..."
+		break
+	else
+		"invalide choice"
+	fi
+done
